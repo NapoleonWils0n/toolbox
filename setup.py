54743cf6d16eabd48c69188a079f0d510f4d144d
Daniel J Wilcox <danieljwilcox@gmail.com>
2018-02-26 16:57:04 +0000

    python toolbox

---
+++ setup.py
@@ -0,0 +1,19 @@
+#from setuptools import setup
+from distutils.core import setup
+
+setup(
+    name='toolbox',
+    version='1.0',
+    description='toolbox of python scripts',
+    url='https://github.com/NapoleonWils0n/toolbox',
+    author='NapoleonWils0n',
+    maintainer='NapoleonWils0n',
+    license='GPL',
+    keywords='python toolbox',
+    packages=['toolbox'],
+    entry_points={
+        'console_scripts': [
+            'm3u8creator = toolbox.m3u8creator:entry',
+        ],
+    }
+)
---
+++ toolbox/m3u8creator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.6
+
+import sys, os, re, os.path, mimetypes
+from urllib.parse import unquote
+
+def main(argv):
+
+    if len(argv) == 0: # no arguments passed to script
+        print("No arguments passed to script")
+        sys.exit()
+    elif len(argv) > 1: # too many arguments passed to script
+        print("Too many arguments passed to script")
+        sys.exit()
+
+    # open text file and use readlines to store in a list
+    def readpage(page):
+        if os.path.isfile(page):
+            if mimetypes.guess_type(page)[0] == 'text/plain':
+                with open(page, 'r') as file:
+                    page = file.readlines()
+                file.close()          
+                return page
+
+    # text file to read in
+    file_input=(argv[0])
+    text=readpage(file_input)
+
+    # m3u8 tags
+    extm3u='#EXTM3U'
+    extint='#EXTINF:0,'
+
+    with open('playlist.m3u8', 'w') as file:
+        file.write("%s\n" % extm3u)
+        for url in text:
+            urlsplit=url[:-5].rsplit('/', 1)[-1] # split url on list / and remove extension
+            vfilename=extint+unquote(unquote(urlsplit)) # url decode and prefix with extint
+            file.write("%s\n" % vfilename)
+            file.write("%s\n" % url)
+    file.close() 
+
+#if __name__ == "__main__":
+#    main(sys.argv[1:])
+
+def entry():
+    main(sys.argv[1:])
