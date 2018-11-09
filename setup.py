56a4a440d9d65d52aa5a96ae44c8e9128a2f2052
Daniel J Wilcox <danieljwilcox@gmail.com>
2018-11-09 00:09:19 +0000

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='toolbox',
-    version='1.1',
+    version='1.2',
     description='toolbox of python scripts',
     url='https://github.com/NapoleonWils0n/toolbox',
     author='NapoleonWils0n',
